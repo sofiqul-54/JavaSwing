11490b1f3a8a8eb86a4802b775d830d836e5233d
sofiqul-54 <sofiqul54@gmail.com>
2018-10-16 12:56:44 +0600

    JavaSwing_CW_01_01

---
+++ Class_Work/chapter_17/src/com/sofiqul54/creatingwindows/Form_01.java
@@ -0,0 +1,21 @@
+package com.sofiqul54.creatingwindows;
+
+import javax.swing.JFrame;
+import javax.swing.UIManager;
+
+public class Form_01 {
+
+    static JFrame aWindow = new JFrame("This is the First Swing Practice");
+
+    @SuppressWarnings("empty-statement")
+    public static void main(String[] args) {
+        int Width = 400;
+        int Height = 150;
+        aWindow.setBounds(450, 300, Width, Height);
+        aWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aWindow.setVisible(true);
+
+        
+    }
+
+}
